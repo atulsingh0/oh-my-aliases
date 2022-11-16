09957331fda0f3d9e9fe63ee3e16789a6e27e1c6
atulsingh0 <victimizeit@gmail.com>
2022-11-15 21:32:34 -0500

    main | added editorconfig

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This repository is my collection of aliases which I find useful in day to day work. Feel free to add/suggest which you find wowsome.
 
 ^The name is inspired from [oh-my-zsh](https://github.com/ohmyzsh/ohmyzsh) which is wowsome utility for zsh users.
+
+
+## Installation
+```
+
+```
--- source-aliases.sh
+++ source-aliases.sh
@@ -1,46 +1,50 @@
 cur_path="$(cd "$(dirname "$0")" && pwd)"
 echo "Loading Aliases from : $cur_path"
 
+function command_exists() {
+  command -v "$@" >/dev/null 2>&1
+}
+
 # General
 source $cur_path/aliases/general.sh
 
 # Docker
-if ! [[ -x $(docker --version 2> /dev/null) ]]; then
+if command_exists docker; then
     [ -f  $cur_path/aliases/docker.sh ] && source $cur_path/aliases/docker.sh
 fi
 
 # K8s
-if ! [[ -x $(kubectl version --short 2> /dev/null) ]]; then
+if command_exists kubectl; then
     [ -f $cur_path/aliases/k8s.sh ] && source $cur_path/aliases/k8s.sh
 fi
 
 # K3d
-if ! [[ -x $(k3d version 2> /dev/null) ]]; then
+if command_exists k3d; then
     [ -f $cur_path/aliases/k3d.sh ] && source $cur_path/aliases/k3d.sh
 fi
 
 # git
-if ! [[ -x $(git --version) ]]; then
+if command_exists git; then
     [ -f $cur_path/aliases/git.sh ] && source $cur_path/aliases/git.sh
 fi
 
 # terraform
-if ! [[ -x $(terraform --version 2> /dev/null ) ]]; then
+if command_exists terraform; then
     [ -f $cur_path/aliases/tform.sh ] && source $cur_path/aliases/tform.sh
 fi
 
 # openshift
-if ! [[ -x $(oc --version 2> /dev/null) ]]; then
+if command_exists oc; then
     [ -f $cur_path/aliases/openshift.sh ] && source $cur_path/aliases/openshift.sh
 fi
 
 # aws
-if ! [[ -x $(aws --version 2> /dev/null) ]]; then
+if command_exists aws; then
     [ -f $cur_path/aliases/aws.sh ] && source $cur_path/aliases/aws.sh
 fi
 
 # podman
-if ! [[ -x $(podman --version 2> /dev/null) ]]; then
+if command_exists podman; then
     [ -f $cur_path/aliases/podman.sh ] && source $cur_path/aliases/podman.sh
 fi
 

455fa0a1479e6a6ffbd277354ecd818ae8fc4a95
atulsingh0 <victimizeit@gmail.com>
2023-01-10 17:12:54 -0500

    main | added general

--- aliases/general.sh
+++ aliases/general.sh
@@ -14,3 +14,11 @@ fi
 # if command -v most >/dev/null 2>&1; then
 #   export PAGER=most
 # fi
+
+alias fs='echo FY$(date -v+11m +%Y) q$((($(date +%m|sed s/^0//)+10)%12/3+1))'
+alias pubip='dig ANY +short @resolver2.opendns.com myip.opendns.com'
+alias urldecode='python3 -c "import sys, urllib as ul; print(ul.unquote_plus(sys.argv[1]))"'
+alias urlencode='python3 -c "import sys, urllib as ul; print(ul.quote_plus(sys.argv[1]))"'
+alias json2yml='python3 -c "import sys, yml, json; print(yaml.safe_dump(json.loads(sys.stdin.read())))"'
+alias yml2json='python3 -c "import sys, yml, json; print(json.safe_dump(yaml.loads(sys.stdin.read())))"'
+alias randstr="cat /dev/urandom | env LC_ALL=C tr -dc 'A-Za-z0-9' | head -c"

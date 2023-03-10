5d3124ad81e4511a5a2c7471d0d9fbe5235732c0
atulsingh0 <victimizeit@gmail.com>
2023-03-10 17:57:09 -0500

    main | Ran shellcheck on k3d.sh

--- aliases/k3d.sh
+++ aliases/k3d.sh
@@ -1,9 +1,11 @@
+#!/bin/sh
+
 ##############################
 # K3d Command aliases
 ##############################
 
-function k3dc() {
-  k3d cluster create $1 --api-port 6550 --servers 1 --agents 3 --port 8080:80@loadbalancer --wait
+k3dc() {
+  k3d cluster create "$1" --api-port 6550 --servers 1 --agents 3 --port 8080:80@loadbalancer --wait
 }
 
 alias k3dd="k3d cluster delete"

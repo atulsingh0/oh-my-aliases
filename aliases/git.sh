d876bfa784eb3c086a0a3dae0273d637688f9e0a
atulsingh0 <victimizeit@gmail.com>
2023-03-23 17:12:00 -0400

    main | updated git

--- aliases/git.sh
+++ aliases/git.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 #################################
 #        git
 #################################
@@ -14,6 +15,7 @@ alias gbc='git branch --show-current'
 alias gco='git checkout'
 alias gacm='git commit -am'
 alias gcma='git commit --amend'
+alias gcf='git commit --fixup'
 alias gdiff='git diff'
 alias glog='git log --oneline --abbrev-commit -30'
 alias gsave='git add -A && git commit -m "chores: save checkpoint"'
@@ -24,43 +26,42 @@ alias gfile='git diff-tree --no-commit-id --name-only -r'
 alias gcme='git commit --allow-empty -m "Trigger Build, Empty commit"'
 alias gaa='git add --all'
 
-function gtst() {
+gtst() {
   git add .
   git commit --amend
   git push -f
 }
 
-function ga() {
-  egrep '<<<<<<< HEAD|>>>>>>>' $@
-  count=$(egrep -c '<<<<<<< HEAD|>>>>>>>' $@ | grep -vc "0$")
+ga() {
+  grep -Er '<<<<<<< HEAD|>>>>>>>' "$@"
+  count=$(grep -Er '<<<<<<< HEAD|>>>>>>>' "$@" | grep -vc "0$")
   # echo $count
-  if [[ $count > 0 ]]; then
-    echo "Fix the merge in "
+  if [ "${count}" -gt 0 ]; then
+    echo "Fix the conflicts."
   else
-    git add $@
+    git add "$@"
   fi
 }
 
-function gcm() {
+gcm() {
   BR=$(git branch --show-current | sed "s_integration/__" | sed -e "s/\(.*[0-9]\).*/\1/g")
   echo "current branch: $BR"
   git commit -m "$BR | $*"
 }
 
-function gfix() {
-  commit=$1
-  files=${@:2}
-
-  git add $files
-  git commit --fixup=$commit
-  git rebase -i --autosquash $commit~
+gfix() {
+  commit="$1"
+  git commit --fixup="${commit}"
+  git stash
+  git rebase -i --autosquash "${commit}"~
+  git stash pop
   echo "Use 'git push -f' to force push the changes."
 }
 
-function gbren() {
+gbren() {
   cur=$(git branch --show-current)
-  new=$1
-  git branch -m $cur $new
+  new="$1"
+  git branch -m "${cur}" "${new}"
   git branch --unset-upstream
-  git push --set-upstream origin :$cur $new
+  git push --set-upstream origin :"${cur}" "${new}"
 }

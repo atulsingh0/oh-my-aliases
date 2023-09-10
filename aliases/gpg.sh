56943a63bfbd64a1c1ba68d6803c184e425e25c7
atulsingh0 <3430610+atulsingh0@users.noreply.github.com>
2023-09-10 19:08:33 -0400

    updated case stmt

--- aliases/gpg.sh
+++ aliases/gpg.sh
@@ -6,25 +6,33 @@
 
 
 gpgl() {
-  if [ -z $1 ]; then 
-    gpg --list-keys
-  fi 
-  
-  if [ $1 == 'l' ]; then 
-    gpg --list-keys --keyid-format=long $2
-  elif [ $1 == 's' ]; then
-    gpg --list-keys --keyid-format=short $2
-  fi
+  case $1 in 
+    l)
+      shift
+      gpg --list-keys --keyid-format=long "$@"
+      ;;
+    s)
+      shift
+      gpg --list-keys --keyid-format=short "$@"
+      ;;
+    *)
+      gpg --list-keys
+      ;;
+  esac
 }
 
 gpgsl() {
-  if [ -z $1 ]; then 
-    gpg --list-secret-keys
-  fi 
-
-  if [ $1 == 'l' ]; then 
-    gpg --list-secret-keys --keyid-format=long $2
-  elif [ $1 == 's' ]; then
-    gpg --list-secret-keys --keyid-format=short $2
-  fi
+  case $1 in 
+    l)
+      shift
+      gpg --list-secret-keys --keyid-format=long "$@"
+      ;;
+    s)
+      shift
+      gpg --list-secret-keys --keyid-format=short "$@"
+      ;;
+    *)
+      gpg --list-secret-keys
+      ;;
+  esac
 }

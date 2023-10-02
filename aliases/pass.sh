c561eea34eb20d3f1d591955bcb4cfd75afd0614
Atul S <3430610+atulsingh0@users.noreply.github.com>
2023-10-02 10:24:15 -0400

    pull for pass alias

--- aliases/pass.sh
+++ aliases/pass.sh
@@ -9,4 +9,4 @@ alias prm='pass rm'
 alias pcp='pass -c'
 alias pgen='pass generate dummy/pass'
 alias ppush='cd ~/.password-store && git push && cd -'
-
+alias ppull='cd ~/.password-store && git pull && cd -'

19db766698f1ac242776383f6d00b76415bd6f11
Atul S <3430610+atulsingh0@users.noreply.github.com>
2023-10-19 12:32:02 -0400

    get user from git token func

--- aliases/git.sh
+++ aliases/git.sh
@@ -200,3 +200,30 @@ gdifc() {
   # Changes done in a commit 
   git diff "$1"~ "$1"
 }
+
+git_get_user() {
+    TOKENS="$1"
+    HOST="$2"
+
+    # setting up hosts
+    if [ -z "$HOST" ]; then
+        API_ENDPOINT="api.github.com"
+        echo "Hostname defaulted to github"
+    else
+        API_ENDPOINT="$HOST/api/v3"
+    fi
+
+    # calling Github API in loop
+    echo ""
+    echo "------------------------------------------------------------------------------"
+    echo "$TOKENS" | tr "," "\n" | while IFS=',' read -r TOKEN; do
+        OUT=$(curl -sL \
+            -H "Accept: application/vnd.github+json" \
+            -H "Authorization: Bearer $TOKEN" \
+            -H "X-GitHub-Api-Version: 2022-11-28" \
+            https://"$API_ENDPOINT"/user)
+        USER=$(echo $OUT | awk -F: '/login/ {print $2}' | tr -d "," | tr -d '"')
+        MASKED_TOKEN=$(mask $TOKEN)
+        echo "$MASKED_TOKEN\t|\t$USER"
+    done
+}

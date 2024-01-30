8c1a06594d157ee96474aa2f0cd7e21032316992
Atul S <3430610+atulsingh0@users.noreply.github.com>
2024-01-29 20:19:53 -0500

    checkpoint: 2024-01-29T20:19:53-05:00

--- source-aliases.sh
+++ source-aliases.sh
@@ -11,8 +11,12 @@ cur_path="$(cd "$(dirname "$0")" && pwd)"
 echo "Loading Aliases from : ${cur_path}"
 echo "Debug: $DEBUG"
 
-command_exists() {
+_command_exists() {
   command -v "$@" >/dev/null 2>&1
+}
+
+enable_plugin() {
+  grep -c "$1" "${PLUGINS}"
 }
 
 log() {
@@ -43,33 +47,33 @@ fi
 ########################################################
 
 # Docker
-if command_exists docker; then
+if enable_plugin git; then
   log "Sourcing docker aliases"
-  [ -f "${cur_path}"/aliases/docker.sh ] && . "${cur_path}"/aliases/docker.sh && log " - done"
+  . "${cur_path}"/aliases/docker.sh && log " - done"
 fi
 
 # K8s
-if command_exists kubectl; then
+if enable_plugin kubectl; then
   log "Sourcing k8s aliases"
-  [ -f "${cur_path}"/aliases/k8s.sh ] && . "${cur_path}"/aliases/k8s.sh && log " - done"
-  [ -f "${cur_path}"/aliases/k8s-plugin.sh ] && . "${cur_path}"/aliases/k8s-plugin.sh && log " - done"
+  . "${cur_path}"/aliases/k8s.sh && log " - done"
+  . "${cur_path}"/aliases/k8s-plugin.sh && log " - done"
 fi
 
-if command_exists helm; then
+if enable_plugin helm; then
   log "Sourcing helm aliases"
-  [ -f "${cur_path}"/aliases/helm.sh ] && . "${cur_path}"/aliases/helm.sh && log " - done"
+  . "${cur_path}"/aliases/helm.sh && log " - done"
 fi
 
 # K3d
-if command_exists k3d; then
+if enable_plugin k3d; then
   log "Sourcing k3d aliases"
-  [ -f "${cur_path}"/aliases/k3d.sh ] && . "${cur_path}"/aliases/k3d.sh && log " - done"
+  . "${cur_path}"/aliases/k3d.sh && log " - done"
 fi
 
 # git
-if command_exists git; then
+if enable_plugin git; then
   log "Sourcing git aliases"
-  [ -f "${cur_path}"/aliases/git.sh ] && . "${cur_path}"/aliases/git.sh && log " - done"
+  . "${cur_path}"/aliases/git.sh && log " - done"
 
   # Enabling Git Ignore
   cat "${cur_path}"/aliases/gitignore_global >"${HOME}"/.oma_gitignore_global
@@ -85,7 +89,7 @@ if command_exists git; then
 fi
 
 # gh github cli
-if command_exists gh; then
+if enable_plugin gh; then
   if [ -n "$GITHUB_TOKEN" ] && [ -n "$GH_HOST" ]; then
     echo "GH_HOST is set: $GH_HOST"
     log "Sourcing gh (github) aliases"
@@ -96,57 +100,57 @@ if command_exists gh; then
 fi
 
 # terraform
-if command_exists terraform; then
+if enable_plugin terraform; then
   log "Sourcing terraform aliases"
-  [ -f "${cur_path}"/aliases/tform.sh ] && . "${cur_path}"/aliases/tform.sh && log " - done"
+  . "${cur_path}"/aliases/tform.sh && log " - done"
 fi
 
 # aws
-if command_exists aws; then
+if enable_plugin aws; then
   log "Sourcing aws aliases"
-  [ -f "${cur_path}"/aliases/aws.sh ] && . "${cur_path}/aliases/aws.sh" && log " - done"
+  . "${cur_path}/aliases/aws.sh" && log " - done"
 fi
 
 # openshift
-if command_exists oc; then
+if enable_plugin oc; then
   log "Sourcing ocp aliases"
-  [ -f "${cur_path}"/aliases/openshift.sh ] && . "${cur_path}"/aliases/openshift.sh && log " - done"
+  . "${cur_path}"/aliases/openshift.sh && log " - done"
 fi
 
 # podman
-if command_exists podman; then
+if enable_plugin podman; then
   log "Sourcing podman aliases"
-  [ -f "${cur_path}"/aliases/podman.sh ] && . "${cur_path}"/aliases/podman.sh && log " - done"
+  . "${cur_path}"/aliases/podman.sh && log " - done"
 fi
 
 # GCP
 if cmd_loc="$(type "gcloud")" || [ -z "${cmd_loc}" ]; then
   log "Sourcing gcloud aliases"
-  [ -f "${cur_path}"/aliases/gcp.sh ] && . "${cur_path}"/aliases/gcp.sh && log " - done"
+  . "${cur_path}"/aliases/gcp.sh && log " - done"
 fi
 
 # ansible
-if command_exists ansible; then
+if enable_plugin ansible; then
   log "Sourcing ansible aliases"
-  [ -f "${cur_path}"/aliases/ansible.sh ] && . "${cur_path}"/aliases/ansible.sh && log " - done"
+  . "${cur_path}"/aliases/ansible.sh && log " - done"
 fi
 
 # vagrant
-if command_exists vagrant; then
+if enable_plugin vagrant; then
   log "Sourcing vagrant aliases"
-  [ -f "${cur_path}"/aliases/vagrant.sh ] && . "${cur_path}"/aliases/vagrant.sh && log " - done"
+  . "${cur_path}"/aliases/vagrant.sh && log " - done"
 fi
 
 # delta
-if command_exists delta; then
+if enable_plugin delta; then
   log "Sourcing delta aliases"
-  [ -f "${cur_path}"/aliases/delta.sh ] && . "${cur_path}"/aliases/delta.sh && log " - done"
+  . "${cur_path}"/aliases/delta.sh && log " - done"
 fi
 
 # asciinema
-if command_exists asciinema; then
+if enable_plugin asciinema; then
   log "Sourcing asciinema aliases"
-  [ -f "${cur_path}"/aliases/asciinema.sh ] && . "${cur_path}"/aliases/asciinema.sh && log " - done"
+  . "${cur_path}"/aliases/asciinema.sh && log " - done"
 fi
 
 # Letsencrypt

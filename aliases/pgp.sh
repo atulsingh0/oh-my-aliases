e939d97d9d5a525387d5340bbfc5c113c399ca9a
Atul S <3430610+atulsingh0@users.noreply.github.com>
2024-04-29 15:00:18 -0400

    few more gpg aliases

--- aliases/pgp.sh
+++ aliases/pgp.sh
@@ -5,34 +5,30 @@
 ## 
 
 
-gpgl() {
-  case $1 in 
-    l)
-      shift
-      gpg --list-keys --keyid-format=long "$@"
-      ;;
-    s)
-      shift
-      gpg --list-keys --keyid-format=short "$@"
-      ;;
-    *)
-      gpg --list-keys
-      ;;
-  esac
-}
+alias gpgl="gpg --list-keys --keyid-format=long"
+alias gpgsl="gpg --list-secret-keys --keyid-format=long"
+alias gpgv="gpg --verify"
+alias gpgc="gpg --clearsign"
+alias gpgenc="gpg --encrypt"
+alias gpgdec="gpg --decrypt"
+alias gpgimp="gpg --import"
+alias gpgexp="gpg --export"
 
-gpgsl() {
-  case $1 in 
-    l)
-      shift
-      gpg --list-secret-keys --keyid-format=long "$@"
-      ;;
-    s)
-      shift
-      gpg --list-secret-keys --keyid-format=short "$@"
-      ;;
-    *)
-      gpg --list-secret-keys
-      ;;
-  esac
+
+get_gpg_algorithm_name() {
+   local n="${1}"
+   local xml=$(curl -X GET -o - -L -s "https://www.iana.org/assignments/openpgp/openpgp.xml")
+   if [ "$?" != "0" ]; then
+     echo -e "unknown algorithm (list not loaded)"
+     return
+   fi
+  
+   local xpath="/reg:registry/reg:registry[@id='openpgp-public-key-algorithms']/reg:record[reg:value='${n}']/reg:description"
+   local name=$( echo -e "${xml}" | xmlstarlet sel -N reg=http://www.iana.org/assignments -t -v "${xpath}" )
+   if [ "$?" != "0" ]; then
+     echo -e "unknown algorithm (number not found)"
+     return
+   fi
+  
+   echo -e "${name}"
 }

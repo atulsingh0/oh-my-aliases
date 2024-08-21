2d02b146f41450becc41a0689b6f0214197eea2f
Atul S <3430610+atulsingh0@users.noreply.github.com>
2024-08-20 23:46:23 -0400

    pass aliases updt

--- aliases/pass.sh
+++ aliases/pass.sh
@@ -10,4 +10,10 @@ alias pcp='pass -c'
 alias pgen='pass generate dummy/pass'
 alias pp1='cd ~/.password-store && git pull && cd -'
 alias pp2='cd ~/.password-store && git pull && git push && cd -'
+
+# https://github.com/roddhjav/pass-update
 alias pupdate='pass update -m'
+
+# https://github.com/atulsingh0/pass-file
+alias pfadd='pass file add'
+alias pfsave='pass file save'

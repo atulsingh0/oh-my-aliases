e0bc72839509940be58cf763b0ae8292fd6d68b5
Atul S <3430610+atulsingh0@users.noreply.github.com>
2025-01-24 17:01:41 +0000

    updated aliases

--- aliases/pass.sh
+++ aliases/pass.sh
@@ -9,7 +9,13 @@ alias prm='pass rm'
 alias pcp='pass -c'
 alias pgen='pass generate dummy/pass'
 alias pp1='cd ~/.password-store && git pull && cd -'
-alias pp2='cd ~/.password-store && git pull && git push && cd -'
+pp2() {
+  cd ~/.password-store || exit
+  git pull && git push origin
+  git checkout master && git merge main && git push origink
+  git checkout main
+  cd - || exit
+}
 
 # https://github.com/roddhjav/pass-update
 alias pupdate='pass update -m'

0a1b3711395d6fbb70b39c4c533383df121ba3ce
Atul S <3430610+atulsingh0@users.noreply.github.com>
2025-02-12 07:16:40 -0500

    moved reload alias

--- aliases/general.sh
+++ aliases/general.sh
@@ -111,16 +111,6 @@ errmsg() {
 }
 
 
-# Reload Aliases
-reload() {
-  case $(basename $SHELL) in
-  zsh) [ -f "${HOME}/.zshrc" ] && source ${HOME}/.zshrc ;; #&& source ${cur_path}/../source-aliases.sh;;
-  bash) [ -f "${HOME}/.bashrc" ] && source ${HOME}/.bashrc ;;
-  *) echo "Unrecognized shell $SHELL" ;;
-  esac
-}
-
-
 # use as - sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 # alias rm_old="rm"

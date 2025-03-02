c9313d6bc35b287c90744f369e15951f5e233ca2
Atul S <3430610+atulsingh0@users.noreply.github.com>
2025-03-01 19:15:48 -0500

    updated limactl alias

--- aliases/lima.sh
+++ aliases/lima.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env sh
+
+alias li='limactl'
+alias listart='limactl start'
+alias listop='limactl stop'
+alias lishell='limactl shell'
+alias lils='limactl list'
+
+lirestart() {
+    limactl stop $1 \
+    && limactl start $1
+}

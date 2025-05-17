f0daa8a934201ad99c4b42f659747a0620905a9c
Atul S <3430610+atulsingh0@users.noreply.github.com>
2025-05-16 22:26:07 -0400

    pass alias update

--- aliases/pass.sh
+++ aliases/pass.sh
@@ -16,3 +16,11 @@ alias pupdate='pass update -m'
 # https://github.com/atulsingh0/pass-file
 alias pfadd='pass file add'
 alias pfsave='pass file save'
+
+
+pgrep() {
+  [ -n "${PASSWORD_STORE_DIR}" ] && PASS_DIR="${PASSWORD_STORE_DIR}" \
+    || PASS_DIR="$HOME/.password-store/"
+
+  tree -P '*'$1'*' "${PASS_DIR}" --prune | grep -v "directories|$PASS_DIR"
+}

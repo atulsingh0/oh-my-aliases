0974e2cc060edab8a896649e333484325e9bc55a
Atul S <3430610+atulsingh0@users.noreply.github.com>
2025-06-14 17:37:53 -0400

    networking alias

--- aliases/networking.sh
+++ aliases/networking.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
 
 alias pubip="curl -s ipinfo.io"
-# alias pubip='dig ANY +short @resolver2.opendns.com myip.opendns.com'
-# alias pubip2="curl ifconfig.me"
-alias pubipv4="curl -s https://api.ipify.org"
-alias pubipv6="curl -s https://api6.ipify.org"
-#alias pubipv4='curl https://ipv4.icanhazip.com/'
 #alias pubip='dig ANY +short @resolver2.opendns.com myip.opendns.com'
+#alias pubip2="curl ifconfig.me"
+alias pubip4="curl -s https://api.ipify.org"
+alias pubip6="curl -s https://api6.ipify.org"
+#alias pubipv4='curl https://ipv4.icanhazip.com/'
 alias pubip2="curl ifconfig.me"
+
+alias localip="ipconfig getifaddr en0"
+
 
 list_open_sockets() {
   find / -type s

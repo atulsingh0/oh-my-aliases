60b31a1aed31b9a2346f587673bffbea555ac2bb
Atul S <3430610+atulsingh0@users.noreply.github.com>
2025-07-01 19:25:36 -0400

    update lima alias

--- aliases/lima.sh
+++ aliases/lima.sh
@@ -5,6 +5,8 @@ alias lcstart='limactl start'
 alias lcstop='limactl stop'
 alias lcshell='limactl shell'
 alias lcls='limactl list'
+alias lcssh=lcshell 
+alias lcshow='limactl show-ssh'
 
 lcrestart() {
     limactl stop $1 \

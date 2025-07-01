93c155aeacdae0712e1a25acc85b3a22a03acf35
Atul S <3430610+atulsingh0@users.noreply.github.com>
2025-07-01 19:57:40 -0400

    checkpoint: 2025-07-01T19:57:40-04:00

--- aliases/lima.sh
+++ aliases/lima.sh
@@ -8,7 +8,15 @@ alias lcls='limactl list'
 alias lcssh=lcshell 
 alias lcshow='limactl show-ssh'
 
-lcrestart() {
+lcreboot() {
+    [ -z $1 ] && echo "Usage: lcreboot <lima_vm_name>" && return;
     limactl stop $1 \
     && limactl start $1
 }
+
+lcip() {
+  [ -z $1 ] && echo "Usage: lcip <lima_vm_name>" && return;
+  limactl shell $1 ip addr show
+}
+
+
